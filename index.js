34e7cd7a8e0cf6a3b72b91a1172ed2b63fafe277
Antoine Neff <9216777+antoineneff@users.noreply.github.com>
2021-11-02 17:03:56 +0100

    feat: create route to get json data, fetch xml ahead of time if needed

--- index.js
+++ index.js
@@ -6,24 +6,47 @@ const server = fastify()
 
 let programs = null
 
-server.get('/', async () => {
-  const TODAY = DateTime.local().setZone('Europe/Paris').toISODate()
+async function updatePrograms() {
+  programs = await fetchAndParseXML()
+}
+
+async function getProgramsOfTheDay(json = false) {
+  const DATE = DateTime.local().setZone('Europe/Paris')
+  const TODAY = DATE.toISODate()
+  const TOMORROW = DATE.plus({ days: 1 }).toISODate()
+
   if (!programs[TODAY]) {
     programs = await fetchAndParseXML()
   }
+  if (!programs[TOMORROW]) {
+    updatePrograms()
+  }
+  if (json) {
+    return programs[TODAY]
+  }
   return print(programs[TODAY])
+}
+
+server.get('/', async () => {
+  const data = await getProgramsOfTheDay()
+  return data
+})
+
+server.get('/json', async () => {
+  const data = await getProgramsOfTheDay(true)
+  return data
 })
 
 server.get('/fetch', async () => {
-  programs = await fetchAndParseXML()
-  return { updated: true  }
+  updatePrograms()
+  return { updating: true }
 })
 
 const start = async () => {
   try {
     await server.listen(3000, '0.0.0.0')
     console.log("API started ✓")
-    programs = await fetchAndParseXML()
+    updatePrograms()
   } catch (error) {
     console.log("Error starting the API", error)
     process.exit(1)
