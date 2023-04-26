9e991cf0c139e28320bc85b137530ebacee157b0
Antoine Neff <9216777+antoineneff@users.noreply.github.com>
2023-04-26 16:20:54 +0200

    chore: add prettier

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-import fastify from 'fastify'
-import { DateTime } from 'luxon'
-import { fetchAndParseXML, print } from './utils.js'
+import fastify from 'fastify';
+import { DateTime } from 'luxon';
+import { fetchAndParseXML, print } from './utils.js';
 
-const server = fastify()
+const server = fastify();
 
-let programs = null
+let programs = null;
 
-async function updatePrograms() {
-  programs = await fetchAndParseXML()
+async function updatePrograms() {
+  programs = await fetchAndParseXML();
 }
 
 async function getProgramsOfTheDay(json = false) {
-  const DATE = DateTime.local().setZone('Europe/Paris')
-  const TODAY = DATE.toISODate()
-  const TOMORROW = DATE.plus({ days: 1 }).toISODate()
+  const DATE = DateTime.local().setZone('Europe/Paris');
+  const TODAY = DATE.toISODate();
+  const TOMORROW = DATE.plus({ days: 1 }).toISODate();
 
   if (!programs[TODAY]) {
-    programs = await fetchAndParseXML()
+    programs = await fetchAndParseXML();
   }
   if (!programs[TOMORROW]) {
-    updatePrograms()
+    updatePrograms();
   }
   if (json) {
-    return programs[TODAY]
+    return programs[TODAY];
   }
-  return print(programs[TODAY])
+  return print(programs[TODAY]);
 }
 
 server.get('/', async () => {
-  const data = await getProgramsOfTheDay()
-  return data
-})
+  const data = await getProgramsOfTheDay();
+  return data;
+});
 
 server.get('/json', async () => {
-  const data = await getProgramsOfTheDay(true)
-  return data
-})
+  const data = await getProgramsOfTheDay(true);
+  return data;
+});
 
 server.get('/fetch', async () => {
-  updatePrograms()
-  return { updating: true }
-})
+  updatePrograms();
+  return { updating: true };
+});
 
 const start = async () => {
   try {
-    await server.listen({ port: 3000, host: '0.0.0.0' })
-    console.log("API started ✓")
-    updatePrograms()
+    await server.listen({ port: 3000, host: '0.0.0.0' });
+    console.log('API started ✓');
+    updatePrograms();
   } catch (error) {
-    console.log("Error starting the API", error)
-    process.exit(1)
+    console.log('Error starting the API', error);
+    process.exit(1);
   }
-}
+};
 
-start()
+start();
